91cbb1acb46842e7bcbed1cf17e61cca8ad41357
Kosta Kondaurov <konstantin.kondaurov@gmail.com>
2022-05-01 22:03:57 +1200

    1sy commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_providers {
+      azurerm ={
+          sosource = "hashicorp/azurerm"
+          version = "~>2.80"
+      }
+    }
+
+    provider "azurerm" {
+        features {}
+    }
+}
+
+resource "azurerm_resource_group" "test_rg" {
+  name = "test_rg"
+  location = "australiaeast"
+}
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+    name = "aks_cluster"
+    location = azurerm_resource_group.test_rg.location
+    resource_group_name = azurerm_resource_group.test_rg.name
+  
+}
