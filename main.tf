8903e7c0cc30458abe0b9fa19e7c316aedd5b0d5
Kosta <konstantin.kondaurov@gmail.com>
2022-05-01 21:02:34 +1000

    TF main typo fix

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.ARM_SUBSDCRIPTION_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
   tenant_id = var.ARM_TENANT_ID
   client_id = var.ARM_CLIENT_ID
   client_secret = var.ARM_CLIENT_SECRET
