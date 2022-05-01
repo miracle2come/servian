fb9ee2629cc26d3ce59c8c3b82c96df6af2519bb
Kosta <konstantin.kondaurov@gmail.com>
2022-05-01 20:41:08 +1000

    add more azure rm provider variables for auth

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ provider "azurerm" {
   features {}
   subscription_id = var.ARM_SUBSDCRIPTION_ID
   tenant_id = var.ARM_TENANT_ID
+  client_id = var.ARM_CLIENT_ID
+  client_secret = var.ARM_CLIENT_SECRET
 }
 
 resource "azurerm_resource_group" "test_rg" {
